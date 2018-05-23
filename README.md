be6882b857afc8f14ad473df8a47ed0a262ecd86
THINK\glyn <glyn@think.com.mt>
2018-05-23 11:04:02 +0200

    updated library

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# client-js
-Javascript client for the Biird Api service.
+Javascript client for the biird Api service.
+
+Download the biird javascript library from here:
+
+https://github.com/biirdio/client-js/blob/master/biird.js
+
+Set up your HTML markup.
+
+<div class="biird" data-biird-id="b9fb0f44-31d5-45df-9ec3-776568802c31"></div>
+
+Add the library to the end of your Body tag. biird will automatically initialize on load.
+
+<script type="text/javascript" src="biird/biird.min.js"></script>
+
+To change any dimension call the Update function and pass the optios as a JSON string. Call the Refresh function right after to update the resources with the new dimensions.
+
+$(function() {
+    biird.update({'language': 'en'});
+    biird.refresh();
+});
--- biird.js
+++ biird.js
@@ -1,56 +1,77 @@
-
-const baseURL = "http://api.biird.io";
-const resourceValueURL = "http://api.biird.io/resourceValue/";
+const baseURL = 'https://api.biird.io';
+const resourceValueURL = 'https://api.biird.io/resourceValue/';
 const biirdOutletSelector = '.biird';
 const dimensionsAttributeSelector = 'data-biird-dimensions';
 const scriptSelector = 'script[data-biird-dimensions]';
-document.addEventListener("DOMContentLoaded", function () {
-    var script = document.querySelectorAll(scriptSelector)[0];
-    let baseDimensionJSON = script.getAttribute(dimensionsAttributeSelector);
-    let baseDimension = {};
-    if (baseDimensionJSON != undefined && baseDimensionJSON.length > 0) {
-        baseDimension = JSON.parse(baseDimensionJSON);
-    }
-    var placeholders = document.querySelectorAll(biirdOutletSelector)
-    for (i = 0; i < placeholders.length; i++) {
-        let counter = i;
-        let placeholder = placeholders[counter];
-        let biirdId = placeholder.getAttribute('data-biird-id');
-        let overrideDimensionJSON = placeholder.getAttribute(dimensionsAttributeSelector);
-        let overrideDimension = {};
-         if (overrideDimensionJSON != undefined && overrideDimensionJSON.length > 0) {
-            overrideDimension = JSON.parse(overrideDimensionJSON)
-        }
-        let finalizedDimensions = {};
-        finalizedDimensions = Object.assign(finalizedDimensions, baseDimension, overrideDimension);
+var biirdBaseDimensions = {};
+var biird = {
+  update: function(newDimensions) {
+    console.log('Updating with ' + JSON.stringify(newDimensions));
 
-        let url = encodeURI(resourceValueURL + biirdId + "?" + attributeParametersFromDimensions(finalizedDimensions));
-       
-        if (placeholder.tagName == "IMG") {
-            placeholder.src = url;
-        } 
-        else if (placeholder.getAttribute('data-biird-background')=='true'){
-            placeholder.style.backgroundImage="url('"+url+"')"
-        }
-        else {
-            retrieveBiirdContent(url, placeholder)
-        }
+    biirdBaseDimensions = newDimensions;
+  },
+  refresh: function() {
+    console.log('Refreshing with ' + JSON.stringify(biirdBaseDimensions));
+    refreshAllBiirdContent(biirdBaseDimensions);
+  }
+};
+
+function refreshAllBiirdContent(baseDimension) {
+  var placeholders = document.querySelectorAll(biirdOutletSelector);
+  for (i = 0; i < placeholders.length; i++) {
+    let counter = i;
+    let placeholder = placeholders[counter];
+    let biirdId = placeholder.getAttribute('data-biird-id');
+
+    let overrideDimensionJSON = placeholder.getAttribute(
+      dimensionsAttributeSelector
+    );
+    let overrideDimension = {};
+    if (
+      overrideDimensionJSON != undefined &&
+      overrideDimensionJSON.length > 0
+    ) {
+      overrideDimension = JSON.parse(overrideDimensionJSON);
     }
-    
-    function retrieveBiirdContent(url, placeholder){
-        let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-            xhr.send();
-            xhr.onload = function () {
-                if (xhr.status === 200) {
-                    placeholder.innerHTML = xhr.responseText;
-                }
-            }
+    let finalizedDimensions = {};
+    finalizedDimensions = Object.assign(
+      finalizedDimensions,
+      baseDimension,
+      overrideDimension
+    );
+    let url = encodeURI(
+      resourceValueURL +
+        biirdId +
+        '?' +
+        attributeParametersFromDimensions(finalizedDimensions)
+    );
+
+    if (placeholder.tagName == 'IMG') {
+      placeholder.src = url;
+    } else if (placeholder.getAttribute('data-biird-background') == 'true') {
+      placeholder.style.backgroundImage = "url('" + url + "')";
+    } else {
+      retrieveBiirdContent(url, placeholder);
     }
-    function attributeParametersFromDimensions(dimensions) {
-        return Object.keys(dimensions).map(function (k) {
-            return encodeURIComponent(k) + '=' + encodeURIComponent(dimensions[k])
-        }).join('&')
+  }
+}
+
+function retrieveBiirdContent(url, placeholder) {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+  xhr.send();
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      placeholder.innerHTML = xhr.responseText;
     }
-})
+  };
+}
+
+function attributeParametersFromDimensions(dimensions) {
+  return Object.keys(dimensions)
+    .map(function(k) {
+      return encodeURIComponent(k) + '=' + encodeURIComponent(dimensions[k]);
+    })
+    .join('&');
+}
